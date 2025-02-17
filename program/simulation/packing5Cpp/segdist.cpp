a51dd0225624460904f4a012ba11e5b26684f0a8
sanhoeeko <806301104@qq.com>
2025-02-17 11:34:06 +0800

    add SIMD segment distance

--- program/simulation/packing5Cpp/segdist.cpp
+++ program/simulation/packing5Cpp/segdist.cpp
@@ -1,59 +1,69 @@
 #include "pch.h"
 #include "segdist.h"
 #include "potential.h"
+#include <immintrin.h>
+#include <cmath>
 
-float cross(const v2 a, const v2 b) {
-    return a.x * b.y - a.y * b.x;
+static inline __m128 _mm_absf(__m128 a) {
+    // Create a mask with all bits set except the sign bit
+    __m128 mask = _mm_castsi128_ps(_mm_set1_epi32(0x7FFFFFFF));
+    // Clear the sign bit of each float in the vector
+    __m128 result = _mm_and_ps(a, mask);
+    return result;
 }
 
-SegmentDist::SegmentDist(float length) : l(length) {
-    mat << 1, 1, 1, -1,
-        1, -1, -1, 1,
-        1, 1, 1, 1,
-        1, -1, -1, -1;
+static inline float min4(float a[4]) {
+    return fminf(fminf(a[0], a[1]), fminf(a[2], a[3]));
 }
 
-float SegmentDist::helper(float abac, float bcba, const v2 ab, const v2 ac)
+static bool isSegmentIntersect(float r, float x1, float y1, float t1, float x2, float y2, float t2)
 {
-    if (abac > 0 && bcba > 0) {
-        return std::abs(cross(ac, ab)) / (2 * l);
-    }
-    else {
-        if (abac < 0) {
-            return ac.norm();  // AC
-        }
-        else { // if bcba < 0
-            return (ac - ab).norm();  // BC
-        }
-    }
+    float c1 = cosf(t1), s1 = sinf(t1), c2 = cosf(t2), s2 = sinf(t2);
+    float x12 = x1 - x2, y12 = y1 - y2;
+    // rectangle intersection
+    if (abs(x12) < r * (abs(c1) + abs(c2)) && abs(y12) < r * (abs(s1) + abs(s2)))return true;
+    // line intersection
+    float rs = abs(r * sinf(t1 - t2)),
+        xsyc1 = abs(x12 * s1 - y12 * c1),
+        xsyc2 = abs(x12 * s2 - y12 * c2);
+    return xsyc1 <= rs || xsyc2 <= rs;
 }
 
-float SegmentDist::inner(float x, float y, float t1, float t2)
+static float segmentDistNoIntersect(float r, float x1, float y1, float t1, float x2, float y2, float t2)
 {
-    float c1 = fcos(t1), c2 = fcos(t2), s1 = fsin(t1), s2 = fsin(t2);
-    float c12 = fcos(t1 - t2);
-    v2 u1 = { l * c1, l * s1 };
-    v2 u2 = { l * c2, l * s2 };
-    v2 r = { x, y };
+    float c1 = cosf(t1), s1 = sinf(t1), c2 = cosf(t2), s2 = sinf(t2);
 
-    Vector4f v1(2 * l * l, 2 * l * x * c1, 2 * l * y * s1, 2 * l * l * c12);
-    Vector4f v2(2 * l * l, 2 * l * x * c2, 2 * l * y * s2, 2 * l * l * c12);
+    // calculate the distance between four pairs of points
+    float ABCD[8], CDBA[8], AC_BD_CB_DA[4];
+    float dx1 = r * c1, dy1 = r * s1, dx2 = r * c2, dy2 = r * s2;
+    ABCD[0] = CDBA[6] = x1 - dx1; ABCD[2] = CDBA[4] = x1 + dx1; 
+    ABCD[1] = CDBA[7] = y1 - dy1; ABCD[3] = CDBA[5] = y1 + dy1; 
+    ABCD[4] = CDBA[0] = x2 - dx2; ABCD[6] = CDBA[2] = x2 + dx2;
+    ABCD[5] = CDBA[1] = y2 - dy2; ABCD[7] = CDBA[3] = y2 + dy2;
+    __m256 ABCDv = _mm256_loadu_ps(ABCD), CDBAv = _mm256_loadu_ps(CDBA);
+    __m256 point_sub = _mm256_sub_ps(ABCDv, CDBAv);
+    __m256 point_mul = _mm256_mul_ps(point_sub, point_sub);
+    __m128 point_sum = _mm256_extractf128_ps(_mm256_hadd_ps(point_mul, point_mul), 0);
+    _mm_store_ps(AC_BD_CB_DA, _mm_hadd_ps(point_sum, point_sum));
+    float min1_sq = min4(AC_BD_CB_DA);
 
-    Vector4f result1 = mat * v1;
-    Vector4f result2 = mat * v2;
+    // calculate the distance between point and line
+    // P: midpoint of AB, Q: midpoint of CD
+    float QQPP[8] = { x2, y2, x2, y2, x1, y1, x1, y1 };
+    float sc[8] = { s1, c1, s1, c1, s2, c2, s2, c2 };
+    float Acd_Bcd_Cab_Dab[4];
+    // QA_QB_PC_PD = [xA - x2, yA - y2, xB - x2, yB - y2, xC - x1, yC - y1, xD - x1, yD - y1]
+    __m256 QA_QB_PC_PD = _mm256_sub_ps(ABCDv, _mm256_loadu_ps(QQPP));
+    __m256 QA_QB_PC_PD_mul = _mm256_mul_ps(QA_QB_PC_PD, _mm256_loadu_ps(sc));
+    __m128 QA_QB_PC_PD_sub = _mm_absf(_mm256_extractf128_ps(_mm256_hsub_ps(QA_QB_PC_PD_mul, QA_QB_PC_PD_mul), 0));
+    _mm_store_ps(Acd_Bcd_Cab_Dab, QA_QB_PC_PD_sub);
+    float min2 = min4(Acd_Bcd_Cab_Dab);
 
-    float abac = result1[0], bcba = result1[1], abad = result1[2], bdba = result1[3];
-    float dcdb = result2[0], cbcd = result2[1], dcda = result2[2], cacd = result2[3];
-
-    float c_ab = helper(abac, bcba, u1 * 2, r - u2 + u1);
-    float d_ab = helper(abad, bdba, u1 * 2, r + u2 + u1);
-    float a_cd = helper(cacd, dcda, u2 * 2, r - u2 + u1);
-    float b_cd = helper(cbcd, dcdb, u2 * 2, r - u2 - u1);
-
-    return std::min({ c_ab, d_ab, a_cd, b_cd });
+    return fminf(sqrtf(min1_sq), min2);
 }
 
-float SegmentDist::operator()(ParticlePair& p)
+float SegDist(float r, float x1, float y1, float t1, float x2, float y2, float t2)
 {
-    return inner(p.x, p.y, p.t1, p.t2);
+    if (isSegmentIntersect(r, x1, y1, t1, x2, y2, t2))return 0.0f;
+    return segmentDistNoIntersect(r, x1, y1, t1, x2, y2, t2);
 }
--- program/simulation/packing5Cpp/segdist.h
+++ program/simulation/packing5Cpp/segdist.h
@@ -1,19 +1,3 @@
 #pragma once
 
-struct v2 { 
-    float x, y; 
-    v2 operator+(const v2& other) const { return { x + other.x, y + other.y }; }
-    v2 operator-(const v2& other) const { return { x - other.x, y - other.y }; }
-    v2 operator*(float scalar) const { return { x * scalar, y * scalar }; }
-    float norm() const { return std::sqrt(x * x + y * y); }
-};
-
-struct SegmentDist {
-    Matrix4f mat;
-    float l;
-
-    SegmentDist(float length);
-    float helper(float abac, float bcba, const v2 ab, const v2 ac);
-    float inner(float x, float y, float t1, float t2);
-    float operator()(ParticlePair& p);
-};
+float SegDist(float r, float x1, float y1, float t1, float x2, float y2, float t2);
