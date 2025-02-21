gradient curve bug fixed
add gradient curve title

program/analysis/qualityCheck.py
@@ -25,6 +25,7 @@ def plotCurves(db: Database, x_name: str, y_name: str):
         xs_ys = flatten(db.apply(ScalarCurve(x_name, y_name)))
         for x, y in xs_ys:
             fig.ax.scatter(x, y, s=2)
+        fig.labels(x_name, y_name)
 
 
 def checkGradient(db: Database):
@@ -43,15 +44,15 @@ def checkEnergy(db: Database):
 
 
 def checkEnergyCurveAt(db: Database, i: int, j: int):
-    plotListOfArray(db[i].simulation_at(j).energyCurve())
+    plotListOfArray(db[i].simulation_at(j).energyCurve(), ('descent steps', 'E/E0'))
 
 
 def checkGradientCurveAt(db: Database, i: int, j: int):
-    plotListOfArray(db[i].simulation_at(j).meanGradientCurve())
+    plotListOfArray(db[i].simulation_at(j).meanGradientCurve(), ('descent steps', 'mean gradient'))
 
 
 def checkMaxGradientCurveAt(db: Database, i: int, j: int):
-    plotListOfArray(db[i].simulation_at(j).maxGradientCurve())
+    plotListOfArray(db[i].simulation_at(j).maxGradientCurve(), ('descent steps', 'max gradient'))
 
 
 def checkLegal(db: Database) -> np.ndarray:

program/simulation/state.py
@@ -28,6 +28,7 @@ class State(ut.HasMeta):
         self.optimizer: Optimizer = None
         self.grid = Grid(self)
         self.gradient = GradientMatrix(self, self.grid)
+        self.ge_valid = False
         self.compile_relaxation_functions()
         # optional objects
         if train:
@@ -55,14 +56,17 @@ class State(ut.HasMeta):
 
     @property
     def mean_gradient_amp(self):
+        if not self.ge_valid: self.refreshGE()
         return self.gradient.sum.G_mean()
 
     @property
     def max_gradient_amp(self):
+        if not self.ge_valid: self.refreshGE()
         return self.gradient.sum.G_max()
 
     @property
     def energy(self):
+        if not self.ge_valid: self.refreshGE()
         return self.gradient.sum.E()
 
     @property
@@ -72,6 +76,16 @@ class State(ut.HasMeta):
     @classmethod
     def random(cls, N, n, d, A, B):
         return cls(N, n, d, A, B, randomConfiguration(N, A, B))
+
+    def calGradient(self):
+        g = self.optimizer.calGradient()
+        self.ge_valid = True
+        return g
+
+    def refreshGE(self):
+        self.grid.gridLocate()
+        self.gradient.calGradientAndEnergy()
+        self.ge_valid = True
 
     def setPotential(self, potential: PotentialBase):
         self.gradient.potential = potential
@@ -91,6 +105,7 @@ class State(ut.HasMeta):
         ker.dll.HollowClear(self.grid.grid.ptr, self.grid.size, ut.max_neighbors)
         self.gradient.zero_grad()
         self.gradient.sum.clear()
+        self.ge_valid = False
 
     def xyt3d(self) -> np.ndarray:
         return self.xyt[:, :3].copy()
@@ -135,7 +150,7 @@ class State(ut.HasMeta):
         self.setOptimizer(0, 0, 1, True)
         gradient_amp = 0
         for t in range(n_steps_init):
-            gradient_amp = self.descent(self.optimizer.calGradient(), step_size_init)
+            gradient_amp = self.descent(self.calGradient(), step_size_init)
             if gradient_amp <= min_grad_init: break
             self.clear_dependency()
         energy = self.CalEnergy_pure()